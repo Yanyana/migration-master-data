a45e1cf3e6dac51a833b92309f533172e2d80584
yanyana <yanyana@github.com>
2023-07-10 11:38:24 +0700

    fix for prebuild

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 require("dotenv").config();
 
-const querydata = process.env.QUERY_TYPE;
+const querydata = process.env.QUERY_TYPE ? process.env.QUERY_TYPE : 'query-site';
 
 const defaultQuery = path.join(
   __dirname,
@@ -17,9 +17,6 @@ if (fs.existsSync(siteQuery)) {
   const siteQueryContent = fs.readFileSync(siteQuery, "utf8");
   fs.writeFileSync(defaultQuery, siteQueryContent, 'utf8');
 } else {
-  const defaultContent = fs.readFileSync(defaultQuery, "utf8");
-
-  fs.writeFileSync(defaultContent, activeLocaleContent, "utf8");
   console.info(
     `Tidak ditemukan template locale untuk "${querydata}", menggunakan template locale default`
   );
